3b942048d75c28c05c840f31e93287b67ab24734
Jop Verhoeven <jop.verhoeven@gmail.com>
2020-01-12 20:47:49 +0100

    Init commit

---
+++ src/main/java/sourcecode/models/profile/Profile.java
@@ -0,0 +1,11 @@
+package sourcecode.models.profile;
+
+import java.util.Date;
+
+public class Profile {
+
+    private String firstname;
+    private String lastname;
+    private Date dateOfBirth;
+
+}
